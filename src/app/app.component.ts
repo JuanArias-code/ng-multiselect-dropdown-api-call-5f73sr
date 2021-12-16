d24aad5d12ac973c8836f345bb41b3fe91e58223
JuanArias-code <hitsugayaalexnina@hotmail.com>
2021-12-16 13:38:36 -0500

    Adaptacion de multiselect a DataGestionRiesgos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,10 +14,24 @@ export class AppComponent {
   dropdownList = [];
   selectedItems = [];
   dropdownSettings = {};
+
+  dropdownList2 = [];
+  selectedItems2 = [];
+  dropdownSettings2 = {};
   ngOnInit() {
     this.getData(); // call service here
+    this.getData2();
 
     this.dropdownSettings = {
+      singleSelection: false,
+      idField: 'item_id',
+      textField: 'item_text',
+      selectAllText: 'Select All',
+      unSelectAllText: 'UnSelect All',
+      itemsShowLimit: 3,
+      allowSearchFilter: true,
+    };
+    this.dropdownSettings2 = {
       singleSelection: false,
       idField: 'item_id',
       textField: 'item_text',
@@ -34,9 +48,20 @@ export class AppComponent {
       .get<any>('https://jsonplaceholder.typicode.com/users')
       .subscribe((data) => {
         for (let i = 0; i < data.length; i++) {
-          tmp.push({ item_id: i, item_text: data[i].name });
+          tmp.push({ item_id: i, item_text: data[i].email });
         }
         this.dropdownList = tmp;
+      });
+  }
+  getData2(): void {
+    let tmp = [];
+    this.http
+      .get<any>('https://jsonplaceholder.typicode.com/posts')
+      .subscribe((data) => {
+        for (let i = 0; i < data.length; i++) {
+          tmp.push({ item_id: i, item_text: data[i].title });
+        }
+        this.dropdownList2 = tmp;
       });
   }
 }
